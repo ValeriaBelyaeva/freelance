211242ffbb74549107e0c16ed4d18ad992aeeb6b
Valeria Belyaeva <62827928+ValeriaBelyaeva@users.noreply.github.com>
2025-06-13 14:33:28 +0300

    naming changes

---
+++ WorkloadChartGenerator/settings.py
@@ -0,0 +1,7 @@
+# settings.py
+
+# Путь к файлу с отчетами
+REPORTS_JSON_PATH = "reports.json"  # путь к файлу с данными
+
+# Параметры по умолчанию для графика
+DEFAULT_SMOOTHING = 3  # степень сглаживания 
